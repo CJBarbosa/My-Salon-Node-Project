f2b934a9933a04639099040b9e9fe1a20a7c4148
Clauber Barbosa <clauberbarbosa@yahoo.com>
2021-12-13 20:06:57 -0600

    update .env

--- _support/nodemailer.js
+++ _support/nodemailer.js
@@ -55,9 +55,9 @@ users.json
       "$oid": "61b6770c45be443ef81171db"
     },
     "email": "jhondoe@email.com",
-    "password": "$2a$10$QHCs1K3t5X9p78aNipyODebBm8qTI9XYT5iqNxs6yJRIxKD/m4YqS",
+    "password": "$2a$10$/WSw/ogfUQKQLRTsW06RouEYVqzjN/t.TUqQG5n55VcPhmkLbAzgS",
     "__v": 0,
     "updatedAt": {
-      "$date": "2021-12-13T21:06:22.307Z"
+      "$date": "2021-12-14T02:04:19.269Z"
     }
   }]
